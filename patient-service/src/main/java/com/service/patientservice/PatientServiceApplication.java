4d0f197004100fe8f6462af712cfd516dc80a255
rohit2thinkitive <rohit.vishwakarma@thinkitive.com>
2022-09-21 20:44:20 +0530

    added swagger ui

--- patient-service/src/main/java/com/service/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/service/patientservice/PatientServiceApplication.java
@@ -2,9 +2,18 @@ package com.service.patientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-
+@Configuration
 @SpringBootApplication
+@EnableSwagger2
 public class PatientServiceApplication {
 
 	public static void main(String[] args) {
@@ -12,5 +21,14 @@ public class PatientServiceApplication {
 
 
 	}
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+
 
 }
